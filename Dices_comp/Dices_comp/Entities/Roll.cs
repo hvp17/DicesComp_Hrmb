ac14812888c58652ef58aab88819a426627e3763
hvp17 <troliukass@gmail.com>
2017-02-22 23:23:28 +0100

    Final commit

---
+++ Dices_comp/Dices_comp/Entities/Roll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dices_comp.Entities
+{
+    public class Roll : AbstractEntity
+    {
+        public DateTime DateStamp { get; set; }
+        public List<int> Dices { get; set; } 
+
+        public string Text { get; set; }
+    }
+}
---
+++ Dices_comp/Dices_comp/HistoryPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dices_comp.Entities;
+using Xamarin.Forms;
+
+namespace Dices_comp
+{
+    public partial class HistoryPage : ContentPage
+    {
+        private ObservableCollection<string> rollSource = new ObservableCollection<string>();
+        public HistoryPage(ObservableCollection<string> rolls)
+        {
+            rollSource = rolls;
+            
+            //InitializeComponent();
+            Inticomp();
+        }
+
+        private void Inticomp()
+        {
+            StackLayout mainLayout = new StackLayout();
+            StackLayout listViewLayout = new StackLayout();
+
+            Button but = new Button();
+            Button clearBtn = new Button();
+
+            ListView historyView = new ListView();
+
+            clearBtn.Text = "Clear";
+            clearBtn.Clicked += (sender, args) => UpdateView();
+
+            but.Text = "Return";
+            but.Clicked += (sender, args) => Return();
+            historyView.ItemsSource = rollSource;
+            listViewLayout.Children.Add(historyView);
+
+            mainLayout.Children.Add(listViewLayout);
+            mainLayout.Children.Add(clearBtn);
+            mainLayout.Children.Add(but);
+
+            Content = mainLayout;
+        }
+
+        private void UpdateView()
+        {
+            
+                rollSource.Clear();
+            
+        }
+
+        private async void Return()
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
